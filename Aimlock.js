ff227e415a135e09a01209947847c84cdfa4ac77
phuc746 <phucchimto4@gmail.com>
2025-03-06 22:01:31 +0700

    Aimlock.js

--- Aimlock.js
+++ Aimlock.js
@@ -1,15 +1,27 @@
-var body = $response.body;
-var obj = JSON.parse(body);
+<script>
+var config = {
+    "DPI": 9999,  // Tăng DPI tối đa
+    "FPS": 999,   // FPS tối đa
+    "MovingSpeed": "MaxSpeed",  // Tốc độ di chuyển nhanh nhất
+    "FrequencyRange": "999Hz",  // Tăng tần số quét
+    "NoRecoil": 1,  // Không giật
+    "Aimlock": 100,  // Aimlock tối đa
+    "Headshot": 100,  // Tăng headshot 100%
+    "Kill": 100,  // Tăng khả năng kết liễu
+    "Sensitivity": 9999,  // Độ nhạy tối đa
+    "AntiBan": 1,  // Chống ban tối đa
+    "AimAssist": 100,  // Hỗ trợ aim tối đa
+    "AutoFire": 1,  // Bắn tự động
+    "HitboxExpand": 100,  // Mở rộng hitbox tối đa
+    "SmoothAim": 1,  // Tăng mượt aim
+};
 
-if (obj.settings) {
-    obj.settings.aim_assist = 1;  // Bật hỗ trợ aim
-    obj.settings.aim_fov = 360;   // Aimlock 360 độ
-    obj.settings.auto_headshot = 1; // Bật auto ghim đầu
-    obj.settings.headshot_rate = 100; // 100% headshot
-    obj.settings.no_recoil = 1;   // Fix rung tâm (không giật)
-    obj.settings.no_spread = 1;   // Fix lố đạn (đạn không lệch)
-    obj.settings.sticky_aim = 1;  // Dính đầu (tâm bám vào đầu)
-    obj.settings.bullet_track = 1; // Đạn tự dò tìm đầu
+function applyConfig() {
+    for (var key in config) {
+        console.log(key + " = " + config[key]);
+    }
 }
 
-$done({ body: JSON.stringify(obj) });
+applyConfig();
+</script>
+
